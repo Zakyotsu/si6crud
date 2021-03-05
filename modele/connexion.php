7b8db2bc6be1a543b2f782496d4dcc99cacfb1c1
unknown <47122315+Zakyotsu@users.noreply.github.com>
2021-03-05 10:54:50 +0100

    Ajout CRUD produit index

--- modele/connexion.php
+++ modele/connexion.php
@@ -1,17 +1,17 @@
 <?php
-$dsn='mysql:host=localhost;dbname=clicom';
-$user='root';
-$password='';
-try {
-	$cnx = new PDO($dsn,$user,$password);
-	// Par défaut PDO désactive la simulation des requêtes préparées
-	$cnx ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-	// Pour générer des erreurs fatales en cas de problème
-	$cnx ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e) {
-	echo 'Une erreur est survenue '.$e->getMessage().'<br/>';
-    die();
-}
+	$dsn='mysql:host=localhost;dbname=clicom';
+	$user='root';
+	$password='';
+	try {
+		$cnx = new PDO($dsn,$user,$password);
+		// Par défaut PDO désactive la simulation des requêtes préparées
+		$cnx ->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		// Pour générer des erreurs fatales en cas de problème
+		$cnx ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch(PDOException $e) {
+		echo 'Une erreur est survenue '.$e->getMessage().'<br/>';
+		die();
+	}
 ?>
 
